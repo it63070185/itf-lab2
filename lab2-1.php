3e93c9c885f08eb63aabb025dbfac5eb30d98d75
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-06 18:28:41 +0700

    Update lab2-1.php

--- lab2-1.php
+++ lab2-1.php
@@ -11,32 +11,32 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-sm-12">
-                    <img src="img/work/work1.jpg" 
+                    <img src="img/work1.jpg" 
                     style="height: 100%; width: 100%;"
                     class="img-fluid"  
                     />
             </div>
             <div class="col-sm-3">
                 <div class="row">
-                    <img src="img/work/work2.jpg" class="img-fluid" 
+                    <img src="img/work2.jpg" class="img-fluid" 
                     style="height: 100%; width: 100%;"/>
                 </div>
                 <div class="row">
-                    <img src="img/work/work3.jpg" class="img-fluid" 
+                    <img src="img/work3.jpg" class="img-fluid" 
                     style="height: 100%; width: 100%;"/>
                 </div>
                 <div class="row">
-                    <img src="img/work/work4.jpg" class="img-fluid" 
+                    <img src="img/work4.jpg" class="img-fluid" 
                     style="height: 100%; width: 100%;"/>
                 </div>
             </div>
             <div class="col-sm-3">
                 <!-- <div class="row"> -->
-                    <img src="img/work/work5.jpg" class="img-fluid" 
+                    <img src="img/work5.jpg" class="img-fluid" 
                     style="height: 66.66%; width: 100%;"/>
                 <!-- </div> -->
                 <div class="row">
-                    <img src="img/work/work6.jpg" class="img-fluid" 
+                    <img src="img/work6.jpg" class="img-fluid" 
                     style="height: 100%; width: 100%;"/>
                 </div>
             </div>
